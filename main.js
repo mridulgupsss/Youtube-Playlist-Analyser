1f6944ebca8eba6163893329feac333b5c976940
Mridul <gmridul556@gmail.com>
2021-10-28 07:59:37 +0530

    error on innerText

--- main.js
+++ main.js
@@ -8,10 +8,26 @@ const link="https://www.youtube.com/playlist?list=PLI_TwOrHUsI8MQNW0BvBAwwHYKgyi
         cpage=pagesArr[0];
         await cpage.goto(link);
         await cpage.waitForSelector("h1#title");
-        
-
-        
+        const name = await cpage.evaluate(function(select){return document.querySelector(select).innerText}, "h1#title");
+        console.log(name);
+        await cpage.waitForSelector("div#stats"); 
+        let Alldata =await cpage.evaluate(getdata, "div#stats");
+        console.log( Alldata.noOfvideos, Alldata.noOfviews, Alldata.date );
     } catch (error) {
         console.log(error);
     }
 })()  // making and calling function at the same place 
+
+
+function getdata(select)
+{
+    let allElem=document.querySelectorAll(select);
+    let noOfvideos = allElem[0].innerText;
+    let noOfviews = allElem[1].innerText;
+    let date = allElem[2].innerText;
+    
+    return{
+        noOfvideos, noOfviews, date
+    }
+
+}
